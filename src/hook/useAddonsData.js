826336c76a6447fdfdf722b5d7242c183a065d2f
Jeff. <jeffreysfu@gmail.com>
2024-12-10 15:45:17 -0400

    Add responsive container styles, implement custom hook for fetching addons data, and create addon selection components

---
+++ src/hook/useAddonsData.js
@@ -0,0 +1,51 @@
+import { useState, useEffect } from 'react'
+
+const useAddonsData = (version) => {
+    const [data, setData] = useState(null)
+    const [isLoading, setIsLoading] = useState(true)
+    const [error, setError] = useState(null)
+
+    // Mapear las URLs a las versiones
+    const urls = {
+        lich: 'https://raw.githubusercontent.com/PentSec/wowAddonsAPI/main/LK/lichking.json',
+        cata: 'https://raw.githubusercontent.com/PentSec/wowAddonsAPI/main/Cata/cataclysm.json',
+        panta: 'https://raw.githubusercontent.com/PentSec/wowAddonsAPI/main/Panda/pandaria.json'
+    }
+
+    useEffect(() => {
+        // Resetear estados al cambiar de versión
+        setData(null)
+        setIsLoading(true)
+        setError(null)
+
+        // Validar si la versión existe
+        const url = urls[version]
+        if (!url) {
+            setError('Versión inválida.')
+            setIsLoading(false)
+            return
+        }
+
+        // Obtener los datos desde la URL
+        fetch(url)
+            .then((response) => {
+                if (!response.ok) {
+                    throw new Error(`Error al obtener los datos: ${response.statusText}`)
+                }
+                return response.json()
+            })
+            .then((jsonData) => {
+                setData(jsonData)
+                setIsLoading(false)
+            })
+            .catch((err) => {
+                setError(err.message)
+                setIsLoading(false)
+            })
+        console.log(`useAddonsData`, data)
+    }, [version])
+
+    return { data, isLoading, error }
+}
+
+export default useAddonsData
---
+++ src/pages/Addons/SelectType.jsx
@@ -0,0 +1,26 @@
+import { Autocomplete, AutocompleteItem } from '@nextui-org/react'
+
+const SelectType = ({ selectedType, setSelectedType, addonTypes }) => {
+    return (
+        <Autocomplete
+            label="Filtrar por tipo"
+            placeholder="Selecciona un tipo"
+            selectedKey={selectedType}
+            onSelectionChange={(key) => {
+                setSelectedType(key)
+            }}
+            defaultItems={addonTypes.map((type) => ({ key: type, name: type }))}
+            radius="sm"
+            size="sm"
+            className="w-full lg:w-36"
+        >
+            {(item) => (
+                <AutocompleteItem key={item.key} value={item.key} textValue={item.name}>
+                    {item.name}
+                </AutocompleteItem>
+            )}
+        </Autocomplete>
+    )
+}
+
+export default SelectType
