d707ed42808093e5ed77bdd78a4ab6b91b40b4b7
Jeff. <jeffreysfu@gmail.com>
2025-01-01 21:57:09 -0400

    Add TypeScript configuration files and type declarations for assets; rename JavaScript files to TypeScript

--- src/maddons.tsx
+++ src/maddons.tsx
@@ -1,28 +1,26 @@
 import ReactDOM from 'react-dom/client'
-import '@/assets/css/tailwind.css'
 import AppRoutes from '@/Routes/Routes'
 import { AppNavbar, AppFooter } from '@/components'
 import { HashRouter as Router, useNavigate, useHref, useLocation } from 'react-router-dom'
 import { NextUIProvider } from '@nextui-org/react'
+import '@/assets/css/tailwind.css'
 import '/logo.png'
 import '/apple-touch-icon.png'
 import '/favicon-16x16.png'
 import '/favicon-32x32.png'
 import '/android-chrome-192x192.png'
 import '/android-chrome-512x512.png'
-import { useTheme } from '@nextui-org/use-theme'
 import 'highlight.js/styles/github-dark.css'
 import 'github-markdown-css/github-markdown.css'
 
 const Maddons = () => {
-    const { theme } = useTheme()
     const navigate = useNavigate()
     const location = useLocation()
     const isLoadingRoute = location.pathname === '/'
     const isErrorRoute = location.pathname === '/404'
 
     return (
-        <NextUIProvider theme={theme} navigate={navigate} useHref={useHref}>
+        <NextUIProvider navigate={navigate} useHref={useHref}>
             <main className="dark:text-foreground dark:bg-background relative flex-grow mx-auto m-0 p-0 min-h-screen items-center justify-center bg-cover bg-center bg-no-repeat bg-fixed dark:bg-maddons-bg bg-maddons-light">
                 <div className="min-h-screen absolute top-0 left-0 right-0 bottom-0 dark:bg-custom-radial z-[1]"></div>
                 <div className="relative z-[2] text-default-900 text-center">
@@ -49,4 +47,4 @@ const AppWrapper = () => {
     )
 }
 
-ReactDOM.createRoot(document.getElementById('root')).render(<AppWrapper />)
+ReactDOM.createRoot(document.getElementById('root')!).render(<AppWrapper />)
