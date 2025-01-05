14ee1ad995347547b605afcd6f22df2fd6cea710
Jeff. <jeffreysfu@gmail.com>
2025-01-05 12:38:39 -0400

    Update ESLint configuration to include @tanstack/query and remove unused code in useDownloadM hook

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -1,9 +1,8 @@
 module.exports = {
-  extends: [
-    'eslint:recommended',
-    'plugin:react/recommended',
-    'plugin:react/jsx-runtime',
-    '@electron-toolkit',
-    '@electron-toolkit/eslint-config-prettier'
-  ]
+    extends: [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'plugin:react/jsx-runtime',
+        'plugin:@tanstack/query/recommended'
+    ]
 }
--- src/hook/useDownloadM.ts
+++ src/hook/useDownloadM.ts
@@ -41,37 +41,3 @@ export const useDownloadM = (): UseDownloadMReturn => {
 
     return { downloadUrl, downloadFile, isPending, error: error?.message || null }
 }
-
-// export default function useDownloadM(): UseDownloadMReturn {
-//     const [downloadUrl, setDownloadUrl] = useState('')
-
-//     useEffect(() => {
-//         async function fetchLatestRelease() {
-//             try {
-//                 const response = await fetch(API_URL)
-//                 const data = await response.json()
-//                 const asset = data.assets.find((a: any) => a.name.endsWith('.exe'))
-//                 setDownloadUrl(asset ? asset.browser_download_url : '#')
-//             } catch (error) {
-//                 console.error('Error fetching the latest release:', error)
-//                 setDownloadUrl('#')
-//             }
-//         }
-
-//         fetchLatestRelease()
-//     }, [])
-
-//     const downloadFile = (url: string): void => {
-//         if (url && url !== '#') {
-//             const a = document.createElement('a')
-//             a.href = url
-//             document.body.appendChild(a)
-//             a.click()
-//             document.body.removeChild(a)
-//         } else {
-//             console.error('Invalid download URL')
-//         }
-//     }
-
-//     return { downloadUrl, downloadFile }
-// }
